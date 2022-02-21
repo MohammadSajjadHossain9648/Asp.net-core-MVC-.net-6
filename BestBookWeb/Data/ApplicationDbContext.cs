08d5c89d33730b08580662a97e784d349fb3d01a
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-02-21 23:39:03 +0600

    Section-2: Add ApplicationDbContext

---
+++ BestBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using BestBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> categories { get; set; }
+        
+    }
+}
