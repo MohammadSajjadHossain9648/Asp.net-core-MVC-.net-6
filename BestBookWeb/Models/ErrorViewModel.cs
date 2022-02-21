76e79d9b09d8a35afb29b598713984cec27b63e5
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-02-22 00:11:43 +0600

    Add project files

---
+++ BestBookWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BestBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
