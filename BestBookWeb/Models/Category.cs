cf68f36f067b2c01dc66e4932dc22116df4ed37d
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-02-22 00:13:48 +0600

    Section-2: Create Db attributes and Range Validation

---
+++ BestBookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BestBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Order must be between 1 to 100!")]
+        public int displayOrder { get; set; }
+        public DateTime createdDateTime { get; set; } = DateTime.Now;
+    }
+}
